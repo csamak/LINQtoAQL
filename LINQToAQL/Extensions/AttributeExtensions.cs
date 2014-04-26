d512a087cb1baf5badc4e44e403a617df4b716bc
Cameron Samak <csamak@apache.org>
2014-04-26 12:04:22 -0700

    Adding a generic method to get values from custom attributes

---
+++ LINQToAQL/Extensions/AttributeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQToAQL.Extensions
+{
+    internal static class AttributeExtensions
+    {
+        /// <remarks>See http://stackoverflow.com/questions/2656189 </remarks>
+        public static TValue GetAttributeValue<TAttribute, TValue>(this Type type,
+            Func<TAttribute, TValue> valueSelector) where TAttribute : Attribute
+        {
+            var att = type.GetCustomAttributes(typeof (TAttribute), true).FirstOrDefault() as TAttribute;
+            return att != null ? valueSelector(att) : default(TValue);
+        }
+    }
+}
