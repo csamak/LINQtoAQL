529a7afe731b09520f2e9e98925d55744a3aa447
Cameron Samak <csamak@apache.org>
2014-04-26 11:21:45 -0700

    Adding Field attribute for overriding inferred field name for the dataset

---
+++ LINQToAQL/DataAnnotations/Field.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LINQToAQL.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class Field : Attribute
+    {
+        public string Name { get; set; }
+    }
+}
