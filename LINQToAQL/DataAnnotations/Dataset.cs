6005f8e1061602231adab2cc054e298e78038bb6
Cameron Samak <csamak@apache.org>
2014-04-26 11:26:17 -0700

    Adding Dataset attribute to override inferred ADB dataset settings

---
+++ LINQToAQL/DataAnnotations/Dataset.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LINQToAQL.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class Dataset : Attribute
+    {
+        public Dataset(string name)
+        {
+            Open = false;
+        }
+
+        public string Name { get; set; }
+        public bool Open { get; set; }
+    }
+}
