af43bf3d07b5050f57ddad1f51fffe635f899e58
Cameron Samak <csamak@apache.org>
2014-05-25 16:04:42 -0700

    Refactoring AQL function calls

--- LINQToAQL/QueryBuilding/AqlFunctionVisitor.cs
+++ LINQToAQL/QueryBuilding/AqlFunctionVisitor.cs
@@ -21,11 +21,11 @@ namespace LINQToAQL.QueryBuilding
         {
             //TODO: Clean this up...
             if (typeof (Math).GetMethods().Where(m => m.Name == "Abs").Contains(expression.Method))
-                SingleArg("numeric-abs", expression.Arguments[0]);
+                AqlFunction("numeric-abs", expression.Arguments[0]);
             else if (typeof (Math).GetMethods().Where(m => m.Name == "Ceiling").Contains(expression.Method))
-                SingleArg("numeric-ceiling", expression.Arguments[0]);
+                AqlFunction("numeric-ceiling", expression.Arguments[0]);
             else if (typeof (Math).GetMethods().Where(m => m.Name == "Floor").Contains(expression.Method))
-                SingleArg("numeric-floor", expression.Arguments[0]);
+                AqlFunction("numeric-floor", expression.Arguments[0]);
                 //Math.Round defaults to MidpointRounding.ToEven, which is equivalent to numeric-round-half-to-even
             else if (
                 new[]
@@ -40,9 +40,9 @@ namespace LINQToAQL.QueryBuilding
                 }.Contains(
                     expression.Method) &&
                  ((ConstantExpression) expression.Arguments[1]).Value.Equals(MidpointRounding.ToEven)))
-                SingleArg("numeric-round-half-to-even", expression.Arguments[0]);
+                AqlFunction("numeric-round-half-to-even", expression.Arguments[0]);
             else if (expression.Method.Equals(typeof (string).GetMethod("ToCharArray", new Type[0])))
-                SingleArg("string-to-codepoint", expression.Object);
+                AqlFunction("string-to-codepoint", expression.Object);
             else if (expression.Method.Equals(typeof (string).GetMethod("get_Chars")))
             {
                 _aqlExpression.Append("string-to-codepoint(");
@@ -52,37 +52,30 @@ namespace LINQToAQL.QueryBuilding
                 _aqlExpression.Append("]");
             }
             else if (expression.Method.Equals(typeof (string).GetMethod("Contains")))
-                ManyArgs("contains", expression.Object, expression.Arguments[0]);
+                AqlFunction("contains", expression.Object, expression.Arguments[0]);
             else if (expression.Method.Equals(typeof (string).GetMethod("StartsWith", new[] {typeof (string)})))
-                ManyArgs("starts-with", expression.Object, expression.Arguments[0]);
+                AqlFunction("starts-with", expression.Object, expression.Arguments[0]);
             else if (expression.Method.Equals(typeof (string).GetMethod("EndsWith", new[] {typeof (string)})))
-                ManyArgs("ends-with", expression.Object, expression.Arguments[0]);
+                AqlFunction("ends-with", expression.Object, expression.Arguments[0]);
             else if (
                 new[]
                 {
                     typeof (string).GetMethod("Join", new[] {typeof (string), typeof (IEnumerable<string>)}),
                     typeof (string).GetMethod("Join", new[] {typeof (string), typeof (string[])})
                 }.Contains(expression.Method))
-                ManyArgs("string-join", expression.Arguments[1], expression.Arguments[0]);
+                AqlFunction("string-join", expression.Arguments[1], expression.Arguments[0]);
             else if (expression.Method.Equals(typeof (string).GetMethod("ToLower", new Type[0])))
-                SingleArg("lowercase", expression.Object);
+                AqlFunction("lowercase", expression.Object);
             else if (expression.Method.Equals(typeof (string).GetMethod("Substring", new[] {typeof (int)})))
-                ManyArgs("substring", expression.Object, expression.Arguments[0]);
+                AqlFunction("substring", expression.Object, expression.Arguments[0]);
             else if (expression.Method.Equals(typeof (string).GetMethod("Substring", new[] {typeof (int), typeof (int)})))
-                ManyArgs("substring", expression.Object, expression.Arguments[0], expression.Arguments[1]);
+                AqlFunction("substring", expression.Object, expression.Arguments[0], expression.Arguments[1]);
             else
                 return false;
             return true;
         }
 
-        protected void SingleArg(string name, Expression argument)
-        {
-            _aqlExpression.AppendFormat("{0}(", name);
-            _visitor.VisitExpression(argument);
-            _aqlExpression.Append(")");
-        }
-
-        protected void ManyArgs(string name, params Expression[] args)
+        protected void AqlFunction(string name, params Expression[] args)
         {
             _aqlExpression.AppendFormat("{0}(", name);
             _visitor.VisitExpression(args[0]);
